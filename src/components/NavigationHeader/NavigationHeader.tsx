49b0d5e7e8e8907dd3b20ea4670f754cf05a7991
“GaliyDima” <dmytro.halii.ki.2020@lpnu.ua>
2024-05-23 13:03:48 +0300

    update classnames

--- src/components/NavigationHeader/NavigationHeader.tsx
+++ src/components/NavigationHeader/NavigationHeader.tsx
@@ -1,5 +1,5 @@
+import React from 'react';
 import logo from "../../assets/Logo.svg";
-
 import "./NavigationHeader.scss";
 
 const HEADER_LINKS = [
@@ -13,21 +13,17 @@ interface NavigationHeaderProps {
   classnames?: string;
 }
 
-const NavigationHeader: React.FC<NavigationHeaderProps> = ({ classnames }) => {
+const NavigationHeader: React.FC<NavigationHeaderProps> = ({ classnames = '' }) => {
   return (
-    <nav
-      className={
-        classnames ? `${classnames} navigation-menu` : "navigation-menu"
-      }
-    >
+    <nav className={`${classnames} navigation-menu`}>
       <div className="navigation-menu__left">
         <img src={logo.src} alt="logo" className="navigation-menu__logo" />
         <p className="navigation-menu__logo-title">AstroVibe</p>
       </div>
       <div className="navigation-menu__right">
         <ul className="navigation-menu__list">
-          {HEADER_LINKS.map((link) => (
-            <li className="navigation-menu__item">
+          {HEADER_LINKS.map((link, index) => (
+            <li key={index} className="navigation-menu__item">
               <a href={link.href} className="navigation-menu__link">
                 {link.label}
               </a>
